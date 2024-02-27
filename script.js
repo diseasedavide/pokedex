002c2a7fb95862d538e28fb5f42fc334aa11c6f2
Davide <rinna.davide@gmail.com>
2024-02-27 08:38:46 +0100

    completata funzionalità tastiera numerica e bottoni rossi. aggiunta gif alla pressione pulsante on

--- script.js
+++ script.js
@@ -28,13 +28,26 @@ const x = document.querySelector(".x");
 const y = document.querySelector(".y");
 const z = document.querySelector(".z");
 const space = document.getElementById("spazio");
-const clear = document.getElementById("cancella")
+const clear = document.getElementById("cancella");
+const on = document.getElementById("on");
+const off = document.getElementById("off");
+const quit = document.getElementById("quit");
+const numero1 = document.querySelector(".numero1");
+const numero2 = document.querySelector(".numero2");
+const numero3 = document.querySelector(".numero3");
+const numero4 = document.querySelector(".numero4");
+const numero5 = document.querySelector(".numero5");
+const numero6 = document.querySelector(".numero6");
+const numero7 = document.querySelector(".numero7");
+const numero8 = document.querySelector(".numero8");
+const numero9 = document.querySelector(".numero9");
+const numero0 = document.querySelector(".numero0");
+const virgola = document.querySelector(".virgola");
 
 async function getRandomPokemon() {
   let randomNumber = Math.floor(Math.random() * 151) + 1;
 
   try {
-    
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/");
     let scelta = nome.value;
     if (nome.value == "") {
@@ -42,7 +55,9 @@ async function getRandomPokemon() {
         "https://pokeapi.co/api/v2/pokemon/" + randomNumber
       );
     } else {
-      response = await fetch("https://pokeapi.co/api/v2/pokemon/" + scelta.toLowerCase());
+      response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/" + scelta.toLowerCase()
+      );
       nome.value = "";
     }
     console.log(response);
@@ -72,228 +87,316 @@ async function getRandomPokemon() {
     pokemonCard.appendChild(pokemonName);
     pokemonCard.appendChild(pokemonImg);
     pokemonContainer.appendChild(pokemonCard);
-
   } catch (error) {
     console.log("Errore");
     const pokemonContainer = document.getElementById("pokemon-container");
     pokemonContainer.innerHTML = "";
-    pokemonContainer.classList.add("container-errore")
+    pokemonContainer.classList.add("container-errore");
     const immagineErrore = document.createElement("img");
     immagineErrore.src = "assets/rocket.gif";
     immagineErrore.classList.add("imgError");
     const btn = document.createElement("button");
     btn.classList.add("btn-remove");
-    btn.textContent ="Oh no! Il tuo Pokémon non è stato trovatoooo... Cliccami per una seconda chance! "
+    btn.textContent =
+      "Oh no! Il tuo Pokémon non è stato trovatoooo... Cliccami per una seconda chance! ";
     pokemonContainer.appendChild(immagineErrore);
-    pokemonContainer.appendChild(btn)
-    
-    
-    btn.addEventListener("click", function() {
-    location.reload();
-    })
+    pokemonContainer.appendChild(btn);
+
+    btn.addEventListener("click", function () {
+      location.reload();
+    });
   }
-  
 }
-
-
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(a.textContent)
-
-
-a.addEventListener("click", function() {
+a.addEventListener("click", function () {
   array.push(a.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-b.addEventListener("click", function() {
+b.addEventListener("click", function () {
   array.push(b.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-c.addEventListener("click", function() {
+c.addEventListener("click", function () {
   array.push(c.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-d.addEventListener("click", function() {
+d.addEventListener("click", function () {
   array.push(d.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-e.addEventListener("click", function() {
+e.addEventListener("click", function () {
   array.push(e.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-f.addEventListener("click", function() {
+f.addEventListener("click", function () {
   array.push(f.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-g.addEventListener("click", function() {
+g.addEventListener("click", function () {
   array.push(g.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-h.addEventListener("click", function() {
+h.addEventListener("click", function () {
   array.push(h.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-i.addEventListener("click", function() {
+i.addEventListener("click", function () {
   array.push(i.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-j.addEventListener("click", function() {
+j.addEventListener("click", function () {
   array.push(j.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-k.addEventListener("click", function() {
+k.addEventListener("click", function () {
   array.push(k.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-l.addEventListener("click", function() {
+l.addEventListener("click", function () {
   array.push(l.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-m.addEventListener("click", function() {
+m.addEventListener("click", function () {
   array.push(m.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-n.addEventListener("click", function() {
+n.addEventListener("click", function () {
   array.push(n.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-o.addEventListener("click", function() {
+o.addEventListener("click", function () {
   array.push(o.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-p.addEventListener("click", function() {
+p.addEventListener("click", function () {
   array.push(p.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-q.addEventListener("click", function() {
+q.addEventListener("click", function () {
   array.push(q.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-r.addEventListener("click", function() {
+r.addEventListener("click", function () {
   array.push(r.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-s.addEventListener("click", function() {
+s.addEventListener("click", function () {
   array.push(s.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-t.addEventListener("click", function() {
+t.addEventListener("click", function () {
   array.push(t.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-u.addEventListener("click", function() {
+u.addEventListener("click", function () {
   array.push(u.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-v.addEventListener("click", function() {
+v.addEventListener("click", function () {
   array.push(v.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-w.addEventListener("click", function() {
+w.addEventListener("click", function () {
   array.push(w.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-x.addEventListener("click", function() {
+x.addEventListener("click", function () {
   array.push(x.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-y.addEventListener("click", function() {
+y.addEventListener("click", function () {
   array.push(y.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-z.addEventListener("click", function() {
+z.addEventListener("click", function () {
   array.push(z.textContent);
-  nome.value = array.join("")
-  console.log(array)
-  console.log(nome.value)
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
 });
 
-clear.addEventListener("click", function() {
-  nome.value=""
+clear.addEventListener("click", function () {
+  nome.value = "";
   array = [];
-})
+});
 
-space.addEventListener("click", function() {
+space.addEventListener("click", function () {
   array.push(" ");
-})
+});
 
+/////////////////////////BOTTONE ON
+on.addEventListener("click", function () {
+  const pokemonContainer = document.getElementById("pokemon-container");
+  pokemonContainer.innerHTML = "";
+  const imgOn = document.createElement("img");
+  imgOn.src = "assets/pikachu-pokemon.gif";
+  imgOn.classList.add("onGif");
+  pokemonContainer.appendChild(imgOn);
+  setTimeout(() => {
+    pokemonContainer.innerHTML =""
+  }, 4000);
+});
 
+off.addEventListener("click", function () {
+  location.reload();
+});
 
+quit.addEventListener("click", function () {
+  location.reload();
+});
+
+numero1.addEventListener("click", function () {
+  array.push(numero1.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero2.addEventListener("click", function () {
+  array.push(numero2.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero3.addEventListener("click", function () {
+  array.push(numero3.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero4.addEventListener("click", function () {
+  array.push(numero4.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero5.addEventListener("click", function () {
+  array.push(numero5.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero6.addEventListener("click", function () {
+  array.push(numero6.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero7.addEventListener("click", function () {
+  array.push(numero7.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero8.addEventListener("click", function () {
+  array.push(numero8.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero9.addEventListener("click", function () {
+  array.push(numero9.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+numero0.addEventListener("click", function () {
+  array.push(numero0.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
+
+virgola.addEventListener("click", function () {
+  array.push(virgola.textContent);
+  nome.value = array.join("");
+  console.log(array);
+  console.log(nome.value);
+});
